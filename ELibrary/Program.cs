a2f6fcfacca8ef216f1acbd906ab580b3d97b20d
Tega Odiete <tegaodiete@gmail.com>
2025-08-31 21:17:58 +0100

    Enabled swagger on all environments

--- ELibrary/Program.cs
+++ ELibrary/Program.cs
@@ -25,11 +25,14 @@ namespace ELibrary
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 

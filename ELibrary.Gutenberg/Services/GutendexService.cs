f831c212b2473dc2a82b0db0d2a78310e0203898
Ibenta Augustine Tochukwu <johnaugustine24@gmail.com>
2025-10-13 18:20:55 +0100

    Add missing using directive for ILogger in GutendexService

--- ELibrary.Gutenberg/Services/GutendexService.cs
+++ ELibrary.Gutenberg/Services/GutendexService.cs
@@ -6,6 +6,7 @@ using ELibrary.Infrastruture.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;

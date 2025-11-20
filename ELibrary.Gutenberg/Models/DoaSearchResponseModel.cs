e2d975c8c5cc3bf2912a5ecca70166bae4da09c8
Alimba Chukwuemeka <alimbaleoo@hotmail.com>
2025-11-20 10:16:13 +0100

    changes to fix image retrival bugs

--- ELibrary.Gutenberg/Models/DoaSearchResponseModel.cs
+++ ELibrary.Gutenberg/Models/DoaSearchResponseModel.cs
@@ -32,7 +32,7 @@ namespace ELibrary.Infrastructure.Models
         public object ParentCommunityList;
 
         [JsonProperty("bitstreams")]
-        public object Bitstreams;
+        public List<BitstreamData> Bitstreams;
 
         [JsonProperty("withdrawn")]
         public string Withdrawn;
@@ -72,4 +72,61 @@ namespace ELibrary.Infrastructure.Models
         [JsonProperty("code")]
         public string Code;
     }
+    internal class BitstreamData
+    {
+        [JsonProperty("uuid")]
+        public string Uuid { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("handle")]
+        public object Handle { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("expand")]
+        public List<string> Expand { get; set; }
+
+        [JsonProperty("bundleName")]
+        public string BundleName { get; set; }
+
+        [JsonProperty("description")]
+        public object Description { get; set; }
+
+        [JsonProperty("format")]
+        public string Format { get; set; }
+
+        [JsonProperty("mimeType")]
+        public string MimeType { get; set; }
+
+        [JsonProperty("sizeBytes")]
+        public int SizeBytes { get; set; }
+
+        [JsonProperty("parentObject")]
+        public object ParentObject { get; set; }
+
+        [JsonProperty("retrieveLink")]
+        public string RetrieveLink { get; set; }
+
+        [JsonProperty("checkSum")]
+        public CheckSum CheckSum { get; set; }
+
+        [JsonProperty("sequenceId")]
+        public int SequenceId { get; set; }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("policies")]
+        public object Policies { get; set; }
+
+        [JsonProperty("link")]
+        public string Link { get; set; }
+
+        [JsonProperty("metadata")]
+        public List<DoaMetaData> Metadata { get; set; }
+    }
+
 }
